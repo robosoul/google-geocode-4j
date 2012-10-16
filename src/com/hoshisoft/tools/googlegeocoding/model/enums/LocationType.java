b7b485c3f091161a0593fc2ac2cfb8082a457ee5
robosoul <nomorestories@gmail.com>
2012-10-16 19:22:23 +0200

    starting...

---
+++ src/com/hoshisoft/tools/googlegeocoding/model/enums/LocationType.java
@@ -0,0 +1,5 @@
+package com.hoshisoft.tools.googlegeocoding.model.enums;
+
+public enum LocationType {
+    ROOFTOP, RANGE_INTERPOLATED, GEOMETRIC_CENTER, APPROXIMATE;
+}
---
+++ src/com/hoshisoft/tools/googlegeocoding/model/enums/Type.java
@@ -0,0 +1,33 @@
+package com.hoshisoft.tools.googlegeocoding.model.enums;
+
+/**
+ * Model: https://developers.google.com/maps/documentation/geocoding/#Types
+ * 
+ * @author Luka Obradovic, 2012
+ *
+ */
+public enum Type {
+    street_address, 
+    route, 
+    intersection, 
+    political, 
+    country, 
+    administrative_area_level_1, 
+    administrative_area_level_2, 
+    administrative_area_level_3, 
+    colloquial_area, 
+    locality, 
+    sublocality, 
+    neighborhood, 
+    premise, 
+    subpremise, 
+    postal_code, 
+    natural_feature, 
+    airport, 
+    park, 
+    point_of_interest,
+    post_box,
+    street_number,
+    floor,
+    room;
+}
